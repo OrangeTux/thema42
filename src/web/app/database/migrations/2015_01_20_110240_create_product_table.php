77793dd9346d17b6aee2903f5d18086926f554f1
Jeroen Kruis <kruisjeroen@gmail.com>
2015-01-20 12:24:30 +0100

    Create user, product and list migrations

---
+++ src/web/app/database/migrations/2015_01_20_110240_create_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function(Blueprint $table)
+		{
+			$table->increments('product_id')->unsigned();
+			$table->string('name');
+			$table->('price');
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('products');
+	}
+
+}
---
+++ src/web/app/database/migrations/2015_01_20_111445_create_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('lists', function(Blueprint $table)
+		{
+			$table->increments('list_id');
+			$table->integer('product_id');
+			$table->integer('quantity');
+
+			$table->timestamps();
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
