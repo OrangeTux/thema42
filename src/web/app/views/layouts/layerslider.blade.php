ef376b71f45ca92cd7dc9d996734a91a5bb1bf06
MalcolmK <malcolm.k.x@gmail.com>
2015-01-21 12:22:07 +0100

    Add homepage

--- src/web/app/views/layouts/layerslider.blade.php
+++ src/web/app/views/layouts/layerslider.blade.php
@@ -3,41 +3,16 @@
 
 		<!-- LayerSlider / Start -->
 		<div id="layerslider" style="width: 940px; height: 400px; margin: 0 auto;">
-
-				<!-- Slide #1 -->
 				<div class="ls-layer" style="transition2d: 67; timeshift: -1000; slidedelay: 7000;">
 
 					<!-- Background -->
-					<img class="ls-bg" src="assets/img/slider-img-01.jpg" alt="" >
+					<img class="ls-bg" src="/assets/img/vegatables.jpg" alt="" >
 
 					<div class="slide-caption ls-s1" style="left: 20px; top: 220px; width: 30%; slidedirection: bottom; slideoutdirection: bottom; durationin: 800; durationout: 800; delayin: 0; delayout: 0;">
-						<h3>This is a caption</h3>
-						<p>Donec scelerisque aliquet mi, non venenatis urnas iaculis. Utea id nila ante. Cras est massa, interdum  ateal imperdiet etean, gravida volutpat hendrerit posuere.</p>
+						<h3>Probeer het zelf!</h3>
 					</div>
 
 				</div>
-
-				<!-- Slide #2 -->
-				<div class="ls-layer" style="transition2d: 3; timeshift: -500; slidedelay: 7000;">
-
-					<!-- Background -->
-					<img class="ls-bg" src="assets/img/slider-img-02.jpg" alt="">
-
-					<div class="slide-caption alt ls-s1" style="left: 0; top: 0; height: 100%; width: 30%; slidedirection: left; slideoutdirection: left; durationin: 800; durationout: 800; delayin: 0; delayout: 0;">
-						<h3>New LayerSlider <br /> With Awesome Features</h3>
-						<p>Donec scelerisque aliquet mi, non venenatis urnas iaculis. Utea id nila ante. Cras est massa, interdum ateal imperdiet etean, gravida eu quame. Aeneana volutpat hendrerit posuere. Aliquam rutrum mattis mollis. Vestibulum a pellentesque tellus.</p>
-					</div>
-
-				</div>
-
-				<!-- Slide #3 -->
-				<div class="ls-layer" style="transition2d: 5; slidedelay: 7000;">
-
-					<!-- Background -->
-					<img class="ls-bg" src="assets/img/slider-img-03.jpg" alt="">
-
-				</div>
-
 		</div>
 		<!-- LayerSlider / End -->
 
--- src/web/app/views/layouts/navbar.blade.php
+++ src/web/app/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 	<div class="sixteen columns">
 		<div id="navigation">
 			<ul id="nav">
-				<li><a id="current" href="{{ URL::route('list.index') }}">Home</a></li>
+				<li><a id="current" href="{{ URL::route('home.index') }}">Home</a></li>
 				<li><a href="{{ URL::route('list.index') }}">Alle lijsten</a></li>
 				<li><a href="{{ URL::route('list.create') }}">Nieuwe lijst</a></li>
 				<li><a>Malcolm Kindermans</a></li>
