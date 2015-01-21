add19de92f46d13598d7c3f2e82b84f9d436aa8f
MalcolmK <malcolm.k.x@gmail.com>
2015-01-21 16:43:33 +0100

    Add sytling page with shoppinglists

--- src/web/app/views/list/index.blade.php
+++ src/web/app/views/list/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-	Show all shoppings lists
+	@foreach($shoppingLists as $shoppingList)
+		<div class="five columns">
+			<div class="large-notice shoppinglist">
+
+				<div class="shoppinglist-actions">
+					<span class="edit">Edit</span>
+					<span class="delete">Delete</span>
+				</div>
+
+				<h3>{{ $shoppingList['title'] }}</h3>
+				<ul>
+					@foreach(array_slice($shoppingList['products'], 0, 8) as $product)
+						<li>
+							<span class="{{ $product['scanned'] ? 'scanned' : "" }}">
+								{{ $product['name'] }} {{ $product['quantity'] }}x
+							</span>
+						</li>
+					@endforeach
+				</ul>
+
+				<div class="shoppinglist-show-more">
+					<span class="show-more"><a href="{{ URL::route('list.index') }}">Toon meer...</a></span>
+				</div>
+			</div>
+		</div>
+	@endforeach
 @stop
