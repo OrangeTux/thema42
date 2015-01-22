3d0591597d9262ab0894586730ba3f8b28dd6b03
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-01-22 17:06:59 +0100

    Load correct HHVM config in web container.

--- src/init/web/hhvm.sh
+++ src/init/web/hhvm.sh
@@ -3,5 +3,8 @@
 export LANG="nl_NL.UTF-8"
 export LC_ALL="nl_NL.UTF-8"
 
-exec hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000
+exec hhvm --mode server \
+    --config /etc/hhvm/php.ini \
+    --config /etc/hhvm/server.ini \
+    -vServer.Type=fastcgi -vServer.Port=9000
 
