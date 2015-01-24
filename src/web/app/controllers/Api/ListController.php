75ff64e8a1c0664cfc985a5cb29ffdd878980325
Jeroen Kruis <kruisjeroen@gmail.com>
2015-01-24 16:54:45 +0100

    Add pivot attributes to return with response

--- src/web/app/controllers/Api/ListController.php
+++ src/web/app/controllers/Api/ListController.php
@@ -9,11 +9,6 @@ use ShoppingList;
 
 class ListController extends BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function index($user_id)
 	{
 		$shoppingLists = User::find($user_id)->shoppingLists;
@@ -21,65 +16,10 @@ class ListController extends BaseController {
 		return $shoppingLists;
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($user_id, $list_id)
 	{
 		$shoppingList = ShoppingList::find($list_id)->products;
 
 		return $shoppingList;
 	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
-
 }
--- src/web/app/models/ShoppingList.php
+++ src/web/app/models/ShoppingList.php
@@ -2,13 +2,13 @@
 
 class ShoppingList extends Eloquent {
 	protected $table = 'shopping_lists';
-	protected $fillable = ['title'];
+	protected $fillable = ['title', 'user_id'];
 
 	public function user() {
 		return $this->belongsTo('User');
 	}
 
 	public function products() {
-		return $this->belongsToMany('Product');
+		return $this->belongsToMany('Product')->withPivot('quantity', 'scanned');
 	}
 }
