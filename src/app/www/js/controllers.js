438aad8a8c42780a84e4d39ff5558af62c1e08cc
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-01-26 10:29:27 +0100

    Setup basic app views.

--- src/app/www/js/controllers.js
+++ src/app/www/js/controllers.js
@@ -1,28 +1,13 @@
-angular.module('starter.controllers', [])
+angular.module('wobbe.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('SignInCtrl', function ($scope, $state) {
+	$scope.signIn = function(user) {
+		console.log('Sign-In', user);
+		$state.go('tabs.home');
+	};
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('HomeTabCtrl', function($scope) {
+	console.log('HomeTabCtrl');
 });
+
