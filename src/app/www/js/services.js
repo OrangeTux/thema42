3070805d0527a8629e6e1fa099cd4091b8924da6
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-01-26 11:56:40 +0100

    Forced commit to pull.

--- src/app/www/js/services.js
+++ src/app/www/js/services.js
@@ -1,4 +1,8 @@
-angular.module('wobbe.services', [])
+angular.module('wobbe.services', ['ngResource'])
 
-;
 
+.factory('Lists', function($resource) {
+    var URL = 'http://private-anon-a27e4c66b-wobbe1.apiary-mock.com/api/v1/'
+
+    return $resource('lists/:id')
+});
