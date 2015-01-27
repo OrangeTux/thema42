b5928ce2580e5d52f4ada2adc17aa21a6a95bc0b
Jeroen Kruis <kruisjeroen@gmail.com>
2015-01-27 14:30:54 +0100

    Add workaround to show products. SOQ: 28015039

--- src/web/app/controllers/Api/ProductController.php
+++ src/web/app/controllers/Api/ProductController.php
@@ -14,109 +14,110 @@ use ShoppingList;
 
 class ProductController extends BaseController {
 
-	public function index() {
-		try
-		{
-			$products = Product::all();
-		}
-		catch (ModelNotFoundException $exception)
-		{
-			return Response::json([
-				'error' => [
-					'message' => 'No products available'
-				]
-			], 404);
-		}
+    public function index() {
+        try
+        {
+            Product::$withoutAppends = true;
+            $products = Product::all();
+        }
+        catch (ModelNotFoundException $exception)
+        {
+            return Response::json([
+                'error' => [
+                    'message' => 'No products available'
+                ]
+            ], 404);
+        }
 
-		return Response::json($products, 200);
-	}
+        return Response::json($products, 200);
+    }
 
-	public function store($listId) {
+    public function store($listId) {
         $validator = Validator::make(Input::all(), ['products' => 'required']);
 
         if ($validator->fails()) {
-			return Response::json([
-				'error' => [
-					'message' => 'Malformed request'
-				]
-			], 400);
-		}
+            return Response::json([
+                'error' => [
+                    'message' => 'Malformed request'
+                ]
+            ], 400);
+        }
 
-		try
-		{
-			$shoppingList = ShoppingList::findOrFail($listId);
-		}
-		catch (ModelNotFoundException $exception)
-		{
-			return Response::json([
-				'error' => [
-					'message' => 'Shopping list does not exist'
-				]
-			], 404);
-		}
+        try
+        {
+            $shoppingList = ShoppingList::findOrFail($listId);
+        }
+        catch (ModelNotFoundException $exception)
+        {
+            return Response::json([
+                'error' => [
+                    'message' => 'Shopping list does not exist'
+                ]
+            ], 404);
+        }
 
-		$products = Input::get('products');
+        $products = Input::get('products');
 
-		foreach ($products as $product) {
-			$productData = [
-				'product_id' => (integer) $product['id'],
-				'quantity' => (integer) $product['quantity'],
-				'scanned' => (integer) false
-			];
+        foreach ($products as $product) {
+            $productData = [
+                'product_id' => (integer) $product['id'],
+                'quantity' => (integer) $product['quantity'],
+                'scanned' => (integer) false
+            ];
 
-			$shoppingList->products()->attach($shoppingList->id, $productData);
-		}
+            $shoppingList->products()->attach($shoppingList->id, $productData);
+        }
 
-		return Response::json($shoppingList->products, 201);
-	}
+        return Response::json($shoppingList->products, 201);
+    }
 
-	public function update($listId, $productId) {
-		$validator = Validator::make(Input::all(), ['product' => 'required']);
+    public function update($listId, $productId) {
+        $validator = Validator::make(Input::all(), ['product' => 'required']);
 
-		if($validator->fails()) {
-			return Response::json([
-				'error' => [
-					'message' => 'Malformed request'
-				]
-			], 400);
-		}
+        if($validator->fails()) {
+            return Response::json([
+                'error' => [
+                    'message' => 'Malformed request'
+                ]
+            ], 400);
+        }
 
-		try
-		{
-			$shoppingList = ShoppingList::findOrFail($listId);
-		}
-		catch (ModelNotFoundException $exception)
-		{
-			return Response::json([
-				'error' => [
-					'message' => 'Shopping list does not exist'
-				]
-			], 404);
-		}
+        try
+        {
+            $shoppingList = ShoppingList::findOrFail($listId);
+        }
+        catch (ModelNotFoundException $exception)
+        {
+            return Response::json([
+                'error' => [
+                    'message' => 'Shopping list does not exist'
+                ]
+            ], 404);
+        }
 
-		$productData = Input::get('product');
+        $productData = Input::get('product');
 
-		$shoppingList->products()->updateExistingPivot($productId, $productData);
+        $shoppingList->products()->updateExistingPivot($productId, $productData);
 
-		return Response::json($shoppingList->products, 200);
-	}
+        return Response::json($shoppingList->products, 200);
+    }
 
-	public function destroy($listId, $productId) {
-		try
-		{
-			$shoppingList = ShoppingList::findOrFail($listId);
-		}
-		catch (ModelNotFoundException $exception)
-		{
-			return Response::json([
-				'error' => [
-					'message' => 'Shopping list does not exist'
-				]
-			], 404);
-		}
+    public function destroy($listId, $productId) {
+        try
+        {
+            $shoppingList = ShoppingList::findOrFail($listId);
+        }
+        catch (ModelNotFoundException $exception)
+        {
+            return Response::json([
+                'error' => [
+                    'message' => 'Shopping list does not exist'
+                ]
+            ], 404);
+        }
 
-		$shoppingList->products()->detach($productId);
+        $shoppingList->products()->detach($productId);
 
-		return Response::json($shoppingList->products, 200);
-	}
+        return Response::json($shoppingList->products, 200);
+    }
 }
--- src/web/app/models/Product.php
+++ src/web/app/models/Product.php
@@ -1,20 +1,30 @@
 <?php
 
 class Product extends Eloquent {
-	protected $table = 'products';
-	protected $hidden = ['pivot'];
-	protected $appends = ['quantity', 'scanned'];	
-	public $timestamps = false;
+    protected $table = 'products';
+    protected $hidden = ['pivot'];
+    protected $appends = ['quantity', 'scanned'];
+    public $timestamps = false;
 
-	public function shoppingLists() {
-		return $this->belongsToMany('ShoppingList');
-	}
+    public static $withoutAppends = false;
 
-	public function getQuantityAttribute() {
-		return $this->pivot->quantity;
-	}
+    public function shoppingLists() {
+        return $this->belongsToMany('ShoppingList');
+    }
 
-	public function getScannedAttribute() {
-		return $this->pivot->scanned; 
-	}
+    protected function getArrayableAppends()
+    {
+        if(self::$withoutAppends){
+            return [];
+        }
+        return parent::getArrayableAppends();
+    }
+
+    public function getQuantityAttribute() {
+        return $this->pivot->quantity;
+    }
+
+    public function getScannedAttribute() {
+        return $this->pivot->scanned;
+    }
 }
